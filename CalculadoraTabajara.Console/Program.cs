08ab03cdd3968c18d9744558d87634a12d6da82e
leonardo Meister <leozinhomm1@gmail.com>
2021-07-30 15:29:39 -0300

    Menu

--- CalculadoraTabajara.Console/Program.cs
+++ CalculadoraTabajara.Console/Program.cs
@@ -18,6 +18,20 @@ namespace CalculadoraTabajara.AppConsole
                 string option = Menu();
                 if (option == "s") break;
 
+                switch (option)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        break;
+                }
             }
 
         }

4e94218afa26ef7ed7e7d45dd978f8c4fb1f4632
leonardo Meister <leozinhomm1@gmail.com>
2021-07-30 14:24:06 -0300

    Implementação da classe calculadora.

---
+++ CalculadoraTabajara.Dominio/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTabajara.Dominio
+{
+    public class Calculadora
+    {
+        public double Somar(double primeiro, double segundo )
+        {
+            return primeiro + segundo;
+        }
+        public double Subrair(double primeiro, double segundo)
+        {
+            return primeiro - segundo;
+        }
+        public double Multiplicar(double primeiro, double segundo)
+        {
+            return primeiro * segundo;
+        }
+        public double Dividir(double primeiro, double segundo)
+        {
+            return primeiro / segundo;
+        }
+    }
+}
